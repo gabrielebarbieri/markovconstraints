5e8dddc2f9d4ea0af8357297b37e4254417cb1ab
gabrielebarbieri <gabriele.barbieri83@gmail.com>
2015-03-06 19:11:55 +0100

    refactor revise + generate_sequence_and_order

--- markovchain/csp.py
+++ markovchain/csp.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import random
-import logging, sys
+import logging
+import sys
 
 __author__ = 'Gabriele'
 
@@ -33,16 +34,16 @@ class ConstraintChain():
         dac_start = datetime.now()
         l = len(self._node_variables)
         for i in xrange(l - 1, 0, -1):
-            self.revise(i - 1, i)
+            self.revise(i)
         self._dac_achieved = True
         self._logger.info("Sequence length: {:}\t(Directed) Arc Consistency: {:}".format(l, datetime.now() - dac_start))
 
-    def revise(self, j, i):
+    def revise(self, i):
         """
         Delete values from the domain of xj until the directed arc (xj,xi) is arc-consistent on the direction j->i
         """
 
-        xj = self._node_variables[j]
+        xj = self._node_variables[i-1]
         xi = self._node_variables[i]
         self._tag += 1
 
@@ -64,7 +65,13 @@ class ConstraintChain():
             vi = [n.value for n in xi]
             raise InconsistentArcException("{:}->{:}".format(vj, vi))
 
-        self._node_variables[j] = filtered
+        self._node_variables[i-1] = filtered
+
+    def get_sequence(self):
+        """
+        Generate a constrained sequence
+        """
+        return self.get_sequence_and_order()[0]
 
     def get_sequence_and_order(self):
         """
@@ -80,12 +87,6 @@ class ConstraintChain():
             orders.append(order)
         self._logger.debug('markov max orders: ' + str(orders))
         return output, orders
-
-    def get_sequence(self):
-        """
-        Generate a constrained sequence
-        """
-        return self.get_sequence_and_order()[0]
 
     def get_item(self, prefix, var):
         """
@@ -131,15 +132,18 @@ if __name__ == '__main__':
     from markov import MarkovTree
     FORMAT = '[%(levelname).1s] %(asctime)s.%(msecs)d %(name)s: %(message)s'
     DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-    logging.basicConfig(level=logging.DEBUG, format=FORMAT, stream=sys.stdout, datefmt=DATE_FORMAT)
+    logging.basicConfig(level=logging.INFO, format=FORMAT, stream=sys.stdout, datefmt=DATE_FORMAT)
 
     mt = MarkovTree(2)
     s = 'missisipix'
     mt.parse(s)
     domain = ['m', 'i', 's', 'p', 'x']
-    variables = [domain for i in xrange(14)]
-    variables.append(['s'])
-    c = ConstraintChain(variables, mt)
+    vs = [domain for k in xrange(14)]
+    vs.append(['s'])
+    c = ConstraintChain(vs, mt)
     c.achieve_dac()
-    for i in xrange(10):
-        seq = c.get_sequence()
+    seqs = [c.get_sequence_and_order() for k in xrange(10)]
+    for seq, ords in seqs:
+        print '  '.join(str(o) for o in ords)
+        print '  '.join(seq)
+        print
