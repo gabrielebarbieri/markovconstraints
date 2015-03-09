e22700a68276021d5aef1b396f0e32401a39cbc2
gabrielebarbieri <gabriele.barbieri83@gmail.com>
2015-03-09 18:55:16 +0100

    csp: implement revise

--- markovchain/csp.py
+++ markovchain/csp.py
@@ -1,25 +1,31 @@
 __author__ = 'gabrielebarbieri'
 import logging
 from datetime import datetime
-import weakref
+from markov import MarkovProcess
 
 
-class Value(object):
+class Value():
 
-    _alphabet = weakref.WeakValueDictionary()
-
-    def __new__(cls, value):
-        obj = Value._alphabet.get(value, None)
-        if not obj:
-            obj = object.__new__(cls)
-            Value._alphabet[value] = obj
-            obj.value = value
-            obj.domains = None
-            obj.flags = None
-        return obj
+    def __init__(self, value, csp_size):
+        self.value = value
+        self.domains = [False] * csp_size
+        self.ins = [False] * csp_size
+        self.outs = [{} for d in xrange(csp_size)]
 
     def __repr__(self):
-        return repr(self.value)
+        return str(self.value)
+
+    def link(self, suffix, pos):
+        self.outs[pos][suffix] = 1
+        suffix.ins[pos + 1] = True
+
+
+def accept(prefix, i):
+    for k, v in enumerate(reversed(prefix)):
+        j = i - k - 1
+        if not v.domains[j]:
+            return False
+    return True
 
 
 class ConstraintChain():
@@ -27,36 +33,40 @@ class ConstraintChain():
     A Constraint Chain: two adjacent variables are constrained by a Markov Constrain.
     """
 
-    def __init__(self, variables, model):
+    def __init__(self, variables, order):
+        self.size = len(variables)
         self.variables = []
-        self._init_variables(variables)
-        self.model = model
+        self.alphabet = {}
+        self.markov_process = MarkovProcess(order)
         self._dac_achieved = False
         self._logger = logging.getLogger(__name__)
+        self._init_variables(variables)
 
     def _init_variables(self, variables):
-        l = len(variables)
         for i, values in enumerate(variables):
             domain = []
-            for item in values:
-                value = Value(item)
-                if not value.domains:
-                    value.domains = [False] * l
-                    value.flags = [-1] * l
+            for v in values:
+                value = self.get_value(v)
                 value.domains[i] = True
                 domain.append(value)
             self.variables.append(domain)
+
+    def get_value(self, v):
+        return self.alphabet.setdefault(v, Value(v, self.size))
+
+    def parse(self, sequence):
+        self.markov_process.parse([self.get_value(v) for v in sequence])
 
     def achieve_dac(self):
         """
         Achieve directional arc consistency. Exploits the chain structure to have optimal constraint propagation
         """
         dac_start = datetime.now()
-        l = len(self.variables)
-        for i in reversed(xrange(self.model.order, l)):
+        for i in reversed(xrange(self.markov_process.order, self.size)):
             self.revise(i)
         self._dac_achieved = True
-        self._logger.info("Sequence length: {:}\t(Directed) Arc Consistency: {:}".format(l, datetime.now() - dac_start))
+        dac_time = datetime.now() - dac_start
+        self._logger.info("Sequence length: {:}\t(Directed) Arc Consistency: {:}".format(self.size, dac_time))
 
     def revise(self, i):
         """
@@ -65,42 +75,54 @@ class ConstraintChain():
         :return:
         """
         for y in self.variables[i]:
-            edges = self.model.transposed[y]
-            for prefix in edges.keys():
-                print i, y,
-                accept = True
-                for j, v in enumerate(reversed(prefix)):
-                    pos = i - j - 1
-                    if not v.domains[pos]:
-                        accept = False
-                        break
-                if accept:
-                    for j, v in enumerate(reversed(prefix)):
-                        pos = i - j - 1
-                        v.flags[pos] = i
-                        print pos, v,
-                print
+            for prefix in self.markov_process.transposed[y]:
+                if accept(prefix, i):
+                    current = y
+                    for k, v in enumerate(reversed(prefix)):
+                        v.link(current, i - k - 1)
+                        current = v
 
-        for k in xrange(self.model.order):
+        # filter domains by excluding non linked values
+        for k in xrange(self.markov_process.order - 1):
             j = i - k - 1
-            if j >= 0:
-                filtered = [v for v in self.variables[j] if v.flags[j] is i]
-                self.variables[j] = filtered
+            self.variables[j] = [v for v in self.variables[j] if v.ins[j] and v.outs[j]]
 
-        print
+        # the case of the small index is special (it does not have incoming links yet)
+        j = i - self.markov_process.order
+        self.variables[j] = [v for v in self.variables[j] if v.outs[j]]
+
+    def __repr__(self):
+        links = []
+        for i, variable in enumerate(self.variables):
+            di = []
+            for value in variable:
+                if value.outs[i].keys():
+                    di.append('{:}->{:}'.format(value, value.outs[i].keys()))
+                else:
+                    di.append(str(value))
+            links.append(str(i) + ' ' + str(di))
+        return '\n'.join(links)
+
+    def get_sequence(self):
+        if not self._dac_achieved:
+            self.achieve_dac()
+        sequence = []
+        for variable in self.variables:
+            prefix = tuple(sequence[-self.markov_process.order:])
+            item = self.markov_process.generate_item(prefix, variable)
+            sequence.append(item)
+
+        return sequence
+
 
 if __name__ == '__main__':
-    from markov import MarkovProcess
-    # from sys import stdout
-    # logging.basicConfig(level=logging.INFO, stream=stdout)
-    seq = 'mississipix'
-    mt = MarkovProcess(2)
-    mt.parse([Value(e) for e in seq])
-    d = ['m', 'i', 's', 'p', 'x']
-    vs = [d for ii in xrange(4)]
-    vs.append(['s'])
-    vs.append(['s'])
-    csp = ConstraintChain(vs, mt)
-    csp.achieve_dac()
-    for var in csp.variables:
-        print var
+
+    d = ['a', 'b', 'c']
+    vs = [d for l in xrange(4)]
+    vs.append(['a'])
+    csp = ConstraintChain(vs, 2)
+    corpus = ['bab', 'aba', 'cba', 'abc']
+    for seq in corpus:
+        csp.parse(seq)
+    for i in xrange(10):
+        print csp.get_sequence()
